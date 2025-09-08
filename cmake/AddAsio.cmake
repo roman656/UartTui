26005129b100d7a031d06491795fc9cde9bdd9f7
roman656 <romanstrah@mail.ru>
2025-09-08 22:44:42 +0300

    fix(CMake): explicit ws2_32 linkage on Windows (asio issue #1590)

--- cmake/AddAsio.cmake
+++ cmake/AddAsio.cmake
@@ -13,7 +13,7 @@ if(NOT TARGET asio::asio AND NOT TARGET asio)
     add_library(asio INTERFACE)
     add_library(asio::asio ALIAS asio)
 
-    # Most compilers do not issue warnings for system code
+    # Most compilers do not issue warnings for system code.
     # You can mark directory as SYSTEM to disable warnings that you can't influence
     target_include_directories(asio INTERFACE SYSTEM "${asio_SOURCE_DIR}/asio/include")
     target_compile_definitions(asio INTERFACE ASIO_STANDALONE ASIO_NO_DEPRECATED)
@@ -21,4 +21,14 @@ if(NOT TARGET asio::asio AND NOT TARGET asio)
 
     find_package(Threads REQUIRED)
     target_link_libraries(asio INTERFACE Threads::Threads)
+
+    # When compiling for Windows using Microsoft Visual C++ or Borland C++,
+    # asio will automatically link in the necessary Windows SDK libraries for sockets support
+    # (i.e. ws2_32.lib and mswsock.lib, or ws2.lib when building for Windows CE).
+    # But for other toolchains (e.g. MSYS2 MinGW/clang) this auto-linking is not provided,
+    # which can lead to undefined reference to '__imp_WSAStartup'.
+    # Therefore link ws2_32 explicitly
+    if(WIN32)
+        target_link_libraries(asio INTERFACE ws2_32)
+    endif()
 endif()
